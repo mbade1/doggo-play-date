bf06e1459260de4d57d38f3f7a32ed9d4b2282df
mbade1 <mbade314@gmail.com>
2020-07-27 18:27:36 -0500

    Updated User relationships, specs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
-
+  has_many :users, through: :parks, source: :playdate
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :parks
-    has_many :commented_playdates, through: :parks, source: :playdate
     has_many :playdates 
-    
+    has_many :commented_playdates, through: :parks, source: :playdates
+
     has_secure_password
 
     validates :username, uniqueness: true, presence: true
