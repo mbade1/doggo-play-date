fe67e790a4f92f5b9b4b73e07bc0a96ab3cd47aa
mbade1 <mbade314@gmail.com>
2020-07-27 18:18:16 -0500

    Styles, login, signup page flexed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'sessions#welcome'
+  root 'sessions#welcome'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
