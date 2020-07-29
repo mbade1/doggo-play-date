9c5ab18bfcfed05d18db5acd3de1b12c94fbbcb5
mbade1 <mbade314@gmail.com>
2020-07-29 17:02:32 -0500

    Comment Model spacing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :park
   belongs_to :user
+
   validates :comment, presence: true
   validates :park_id, presence: true
   validates :user_id, presence: true
-
 end
