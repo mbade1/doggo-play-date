378ef98a358dccc35355d8fa237c2fbfdb3e3420
mbade1 <mbade314@gmail.com>
2020-07-30 17:29:00 -0500

    Sorted and Current Date in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,30 +7,9 @@ module ApplicationHelper
     
     def current_date(dates)
         dates.each do |date|
-            if date > Time.now
+            if Date.parse(date.date.to_s).future?
                 date
             end
         end
     end
 end
-
-#USER SHOW/HOMEPAGE FOR LOGGED IN USER
-#<% @user.playdates.each do |playdate| %>
-#     <tr>
-#       <td><%= link_to "#{playdate.park.name}", park_path(playdate.park) %></td>
-#       <td><%= playdate.date.strftime("%B %d, %Y") %></td>   
-#       <td><%= playdate.time.strftime("%I:%M %P") %></td>
-#     </tr>
-
-
-#PARK SHOW
-# <% @park.playdates.each do |playdate| %>
-#     <tr>
-#       <td><%= playdate.date.strftime("%B %d, %Y") %></td> 
-#       <td><%= playdate.time.strftime("%I:%M %P") %> </td>
-#       <td><%= playdate.user.username %></td>
-#       <td><%= playdate.user.family_size %></td>
-#     </tr>
-#   <% end %>
-
-#ADD HELPER METHOD FOR DATE BEING AFTER CURRENT DATE!!!
