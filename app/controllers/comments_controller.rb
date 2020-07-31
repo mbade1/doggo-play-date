98cb312c30a5bd616fb26b3a3300dd0de4b0254f
mbade1 <mbade314@gmail.com>
2020-07-31 17:51:09 -0500

    Form for Comments Edit and New

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
     @park = Park.find_by(:id => params[:park_id])
   end 
 
-
   def current_park
     if session[:park_id].present?
       park = Park.find_by(:park_id => session[:park_id])
@@ -23,29 +22,33 @@ class CommentsController < ApplicationController
     end
   end
 
-def edit
-    @park = Park.find(params[:park_id])
+  def edit
+    @park = Park.find_by(:id => params[:park_id])
     @comment = Comment.find_by(:id => params[:id])
-end
+  end
 
-def update
+  def update
     @comment = Comment.find_by(:id => params[:id])
+    @park = Park.find_by(:id => params[:park_id])
     if @comment.update(comment_params)
       @park = @comment.park
       flash[:message] = "Your comment has been updated!"
       redirect_to park_path(@park)
+    else
+      render :edit
     end
-end
+  end
 
-def destroy
-  @comment = Comment.find_by(:id => params[:id]) 
-  @comment.destroy
-  @park = Park.find_by(:id => params[:park_id])
-  flash[:message] = "Comment has been deleted!"
-  redirect_to park_path(@park)
-end
+  def destroy
+    @comment = Comment.find_by(:id => params[:id]) 
+    @comment.destroy
+    @park = Park.find_by(:id => params[:park_id])
+    flash[:message] = "Comment has been deleted!"
+    redirect_to park_path(@park)
+  end
 
   private
+  
   def comment_params
     params.require(:comment).permit(:comment, :park_id, :user_id, :park_review)
   end
