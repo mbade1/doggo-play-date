d96ec5d75c3dbc19e0334b3587d128038faa11ce
mbade1 <mbade314@gmail.com>
2020-07-31 17:43:17 -0500

    _form for Playdates

--- NOTES.md
+++ NOTES.md
@@ -1,8 +1,5 @@
 FRIDAY
     - OAuth linked with Facebook
-    - DB:
-        Comment: Add Rating System
-        Park: Change Location to Address?
     - Sort by method for order of play dates
     - Refactor:
         Make new/edit partials for playdates and comments
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :park
   belongs_to :user
 
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { minimum: 15 }
   validates :park_id, presence: true
   validates :user_id, presence: true
   validates :park_review, presence: true
