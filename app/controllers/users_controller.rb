a8d5ebbd9c176178388f201e8278247db59a4158
mbade1 <mbade314@gmail.com>
2020-08-01 10:53:02 -0500

    User can update family size

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,42 @@
 class UsersController < ApplicationController
-  before_action :require_login, except: [:new, :create]
+  before_action :require_login, except: [:new, :create, :update]
 
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(current_user.id)
+    else
+      render :new
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to user_path(current_user.id)
-        else
-          render :new
-        end
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find_by(:id => params[:id])
+    if @user.update(user_params)
+      flash[:message] = "Your family size has been updated!"
+      redirect_to user_path(current_user)
+    else
+      render :edit
     end
+  end
 
-    def show
-        @user = User.find_by(:id => params[:id])      
-    end
+  def show
+    @user = User.find_by(:id => params[:id])      
+  end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:username, :password, :family_size)
-    end
+  def user_params
+    params.require(:user).permit(:username, :password, :family_size)
+  end
+
 end
