fbbde61867cf459d2aac3b46d9e528be3f3bb513
mbade1 <mbade314@gmail.com>
2020-08-03 12:59:05 -0500

    User Views Cleaned

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -3,7 +3,6 @@ class Playdate < ApplicationRecord
     belongs_to :user
 
     validates :date, presence: true
-    scope :date, ->(date) { where("date < ?", date) if date.present? }
     validates :time, presence: true 
 
 end
