bcb191a4761a3c119054a5ddbd22fd5faca81276
mbade1 <mbade314@gmail.com>
2020-08-02 20:18:32 -0500

    Park Controller clean

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,35 +1,35 @@
 class ParksController < ApplicationController
-    before_action :require_login, except: [:show]
+  before_action :require_login, except: [:show]
 
-    def index
-        @parks = Park.all
+  def index
+      @parks = Park.all
+  end
+
+  def show 
+    @park = Park.find_by(:id => params[:id])
+    @comments = Comment.all
+    @comment = Comment.new
+    @users = User.all
+  end
+
+  def new
+    @park = Park.new
+  end
+
+  def create
+    @park = Park.new(park_params)
+    if @park.save
+      flash[:message] = "Your park has been added!"
+      redirect_to park_path(@park)
+    else
+      render :new
     end
+  end
 
-    def show 
-        @park = Park.find_by(:id => params[:id])
-        @comments = Comment.all
-        @comment = Comment.new
-        @users = User.all
-    end
+  private
 
-    def new
-        @park = Park.new
-    end
-
-    def create
-        @park = Park.new(park_params)
-        if @park.save
-          flash[:message] = "Your park has been added!"
-          redirect_to park_path(@park)
-        else
-          render :new
-        end
-    end
-
-    private
-
-    def park_params
-        params.require(:park).permit(:name, :location, :hound, :description)
-    end
+  def park_params
+    params.require(:park).permit(:name, :location, :hound, :description)
+  end
 end
 
