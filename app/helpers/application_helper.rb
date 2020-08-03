3a7688170788745648ab72f3978bdee505d85ce1
mbade1 <mbade314@gmail.com>
2020-08-02 20:22:37 -0500

    Application Helper clean

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-    def sort_by_date(dates)
-        dates.sort do |a, b| 
-            a <=> b
-        end
+  def sort_by_date(dates)
+    dates.sort do |a, b| 
+      a <=> b
     end
+  end
     
-    def current_date(dates)
-        dates.each do |date|
-            if Date.parse(date.date.to_s).future?
-                date
-            end
-        end
+  def current_date(dates)
+    dates.each do |date|
+      if Date.parse(date.date.to_s).future?
+        date
+      end
     end
+  end
 end
