af50df257980fdcc9281d7654426e6c9a1037d3f
mbade1 <mbade314@gmail.com>
2020-08-02 20:21:32 -0500

    Playdates Controller clean

--- app/controllers/playdates_controller.rb
+++ app/controllers/playdates_controller.rb
@@ -1,58 +1,58 @@
 class PlaydatesController < ApplicationController
 
-    def index
-        @parks = Park.all
-        @playdate = Playdate.all
-    end
+  def index
+    @parks = Park.all
+    @playdate = Playdate.all
+  end
     
-    def new
-        @park = Park.find(params[:park_id])
-        @playdate = Playdate.new
+  def new
+    @park = Park.find(params[:park_id])
+    @playdate = Playdate.new
+  end
+
+  def create
+    @playdate = Playdate.new(playdate_params)
+    @park = @playdate.park
+    if @playdate.save
+      flash[:message] = "Your playdate has been added!"
+      redirect_to park_playdate_path(@park, @playdate)
+    else
+      render :new
     end
+  end
 
-    def create
-        @playdate = Playdate.new(playdate_params)
-        @park = @playdate.park
-        if @playdate.save
-            flash[:message] = "Your playdate has been added!"
-            redirect_to park_playdate_path(@park, @playdate)
-        else
-            render :new
-        end
-    end
+  def show
+    @playdate = Playdate.find_by(:id => params[:id])
+    @user = @playdate.user.username
+    @park = Park.find_by(:id => params[:park_id])
+  end
 
-    def show
-        @playdate = Playdate.find_by(:id => params[:id])
-        @user = @playdate.user.username
-        @park = Park.find_by(:id => params[:park_id])
-    end
+  def edit
+    @park = Park.find_by(:id => params[:park_id])
+    @playdate = Playdate.find_by(params[:id])
+  end
 
-    def edit
-        @park = Park.find_by(:id => params[:park_id])
-        @playdate = Playdate.find_by(params[:id])
-    end
+  def update
+    @playdate = Playdate.find_by(:id => params[:id])
+    @playdate.update(playdate_params)
+    @park = @playdate.park
+    flash[:message] = "Your playdate has been updated!"
+    redirect_to park_playdate_path(@park, @playdate)
+  end    
 
-    def update
-        @playdate = Playdate.find_by(params[:id])
-        @playdate.update(playdate_params)
-        @park = @playdate.park
-        flash[:message] = "Your playdate has been updated!"
-        redirect_to park_playdate_path(@park, @playdate)
-    end    
+  def destroy
+    @playdate = Playdate.find_by(:id => params[:id]) 
+    @playdate.destroy
+    @park = Park.find_by(:id => params[:park_id])
+    flash[:message] = "Playdate has been deleted!"
+    redirect_to park_path(@park)
+  end
 
-    def destroy
-        @playdate = Playdate.find_by(:id => params[:id]) 
-        @playdate.destroy
-        @park = Park.find_by(:id => params[:park_id])
-        flash[:message] = "Playdate has been deleted!"
-        redirect_to park_path(@park)
-      end
+  private
 
-    private
-
-    def playdate_params
-        params.require(:playdate).permit(:time, :date, :user_id, :park_id)
-    end
+  def playdate_params
+    params.require(:playdate).permit(:time, :date, :user_id, :park_id)
+  end
 
 end
 
