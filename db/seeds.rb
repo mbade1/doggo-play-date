8c7a8803cd25069c2fa1e7cfc75e367f90b61e73
mbade1 <mbade314@gmail.com>
2020-08-11 17:00:08 -0500

    Final Updates before review

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,15 @@ user_2 = User.create(username: "Rudy", password: "password", uid: nil, family_si
 user_3 = User.create(username: "Sandra", password: "password", uid: nil, family_size: 5)
 
 
-park_1 = Park.create(name: "Park Lane Park", location: "114 Park Lane", hound: "Doggo", description: "A beautiful park.")
-park_2 = Park.create(name: "Better than Park Lane Park Park", location: "1000 Park Avenue", hound: "Pupper", description: "A park for puppers.")
-park_3 = Park.create(name: "Doggo Central", location: "1234 Dog City", hound: "All", description: "Bounce House for Dogs")
-park_4 = Park.create(name: "Big Dogz Only", location: "1234 Big Dog City", hound: "Doggo", description: "Huge dogs only - no wimps.")
-park_5 = Park.create(name: "Secret Agent Pups Only", location: "001 Bond Drive", hound: "Pupper", description: "James Bond themed dog park")
-park_6 = Park.create(name: "Jumpin Puppers and Doggos Park",location: "2000 Trampoline Drive", hound: "All", description: "Trampoline Park for Dogs")
+park_1 = Park.create(name: "Norwood Estate Dog Park", location: "1009 Edgecliff Terrace", hound: "All", description: "Fenced-off dog park with some park benches, tree shade, water supply & drinking bowls.")
+park_2 = Park.create(name: "Red Bud Isle", location: "3401 Redbud Trail", hound: "Pupper", description: "Lakefront off-leash dog park with 13 acres of recreational space, plus hiking, canoeing & kayaking.")
+park_3 = Park.create(name: "Great Northern Dog Park", location: "7099 Great Northern Blvd", hound: "All", description: "Large off-leash dog area featuring walking paths, a retention pond & grassy open areas.")
 
-playdate_1 = Playdate.create(date: "2020-01-01", time: '-12:00', park_id: 1, user_id: 1)
-playdate_2 = Playdate.create(date: "2020-05-01", time: '-24:00', park_id: 1, user_id: 1)
-playdate_3 = Playdate.create(date: "2020-07-01", time: '-23:00', park_id: 1, user_id: 2)
-playdate_4 = Playdate.create(date: "2020-07-19", time: '-19:00', park_id: 2, user_id: 2)
-playdate_5 = Playdate.create(date: "2022-01-05", time: '-18:00', park_id: 2, user_id: 3)
+playdate_1 = Playdate.create(date: "2020-08-20", time: '-12:00', park_id: 1, user_id: 1)
+playdate_2 = Playdate.create(date: "2020-09-23", time: '-24:00', park_id: 1, user_id: 1)
+playdate_3 = Playdate.create(date: "2020-08-14", time: '-23:00', park_id: 1, user_id: 2)
+playdate_4 = Playdate.create(date: "2020-08-26", time: '-19:00', park_id: 2, user_id: 2)
+playdate_5 = Playdate.create(date: "2022-01-25", time: '-18:00', park_id: 2, user_id: 3)
 playdate_6 = Playdate.create(date: "2022-01-01", time: '-14:00', park_id: 3, user_id: 3)
 
 comment_1 = Comment.create(comment: "Wow! What a beautiful park!", park_id: 1, user_id: 1, park_review: 4)
